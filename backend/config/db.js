10af86b677e425d13131920e6de49e8462b3088c
Jayce <pltbao23@clc.fitus.edu.vn>
2025-12-15 18:16:11 +0700

    Add mongodb cloud connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = async () => {
+  console.log(process.env.MONGO_DB_URI);
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/smart_bulb_db');
+    await mongoose.connect(process.env.MONGO_DB_URI);
     console.log('MongoDB Connected Successfully!');
   } catch (err) {
     console.error('MongoDB Connection Failed:', err.message);
